196bfdf1484cdacaf6b658226d56dcad696bbf20
rita <runyalen88@duck.com>
2025-03-07 17:48:25 +0100

    .h v1 structs

---
+++ philo/philo.h
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 16:02:53 by urlooved          #+#    #+#             */
+/*   Updated: 2025/03/07 17:48:07 by urlooved         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <limits.h>
+# include <unistd.h>	// for usleep() & write
+# include <stdio.h>		// malloc
+# include <sys/time.h>	// ??
+# include <stdbool.h>	// because i love bools
+
+
+//	------------------
+//	::  parameters  ::
+//	------------------
+
+//	---------------
+//	::  Structs  ::
+//	---------------
+typedef struct	s_philo	t_philo;
+
+typedef struct s_table
+{
+	time_t				start_meeting_at;				// meeting created_at
+	unsigned int		amount_philos;
+	time_t				t_t_die;						// amount ms of time to die
+	time_t				t_t_eat;						// amount ms of time to eat
+	time_t				t_t_sleep;						// amount ms of time to sleep
+	unsigned int		min_amount_meals;				// min amount of meals that every philo must eat before end of process
+	pthread_t			t_stop_program;					// a thread that will be checking is we need to stop the program
+	bool				sim_should_stop;				// BOOL true/false 
+	pthread_mutex_t		sim_stop_lock;					// TODO check if necesary
+	pthread_mutex_t		write_lock;						// mutex to manage wich thread can write or not.
+	pthread_mutex_t		*fork_locks;					// mutex that manage the fork handling to every philo
+	t_philo				**philos;						// array of philos
+} t_table; 
+
+typedef struct s_philo
+{
+	unsigned int		id;							// Id of each philosopher
+	pthread_t			thread_id;					// thread_id of each philo
+	unsigned int		times_ate;					// amount of meals per philo
+	unsigned int		fork[2];					// max amount of forks per philo
+	pthread_mutex_t		meal_time_lock;				// time to eat ?
+	time_t				last_meal_at;				// timestamp that record last meal eaten (usefull to know if any philo already died)
+	t_table				*table;
+} t_philo;
+
+//	-------------
+//	::  Enums  ::
+//	-------------
+typedef enum e_status
+{
+	DIED = 0,
+	EATING = 1,
+	SLEEPING = 2,
+	THINKING = 3,
+	GOT_FORK_1 = 4,
+	GOT_FORK_2 = 5
+}	t_status;
+
+
+
+
+//	----------------
+//	::  Handlers  ::
+//	----------------
+
+//	-------------
+//	::  Tools  ::
+//	-------------
+
+//	------------
+//	::  Free  ::
+//	------------
+
+
+
+
+
+
+
+#endif
