3d78c7bb1179de7aedf8d8effdb5f5cd28f35a5a
rita <runyalen88@duck.com>
2025-03-10 17:57:30 +0100

    update parser

--- philo/parser.c
+++ philo/parser.c
@@ -6,7 +6,7 @@
 /*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:10:19 by urlooved          #+#    #+#             */
-/*   Updated: 2025/03/08 17:02:38 by urlooved         ###   ########.fr       */
+/*   Updated: 2025/03/10 17:43:38 by urlooved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,16 @@ bool	are_inputs_valid(int ac, char **av)
 	int	i;
 	int	nb;
 
+
 	i = 1;												// We start i=1 to avoid the first arg (./philo)
 	while (i < ac)
 	{
 		if (!has_only_digits(av[i]))
 			return(printf("wrong inputs format\n"), false);
 		nb = nbs_atoi(av[i]);
-		if ((i == 1 && nb <= 0) || nb == -1)
+		if (i == 1 && (nb <= 0 || nb > 200))
 			return(printf("wrong amount of philos my dear\n"), false);
-		if (nb == -1)
+		if (i > 1 && nb < 60)
 			return(printf("invalid inputs format my dear\n"), false);
 		i++;
 	}
