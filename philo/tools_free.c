e445306ff7656a0fe512f24da2a38aad6e46b897
rita <runyalen88@duck.com>
2025-03-10 11:29:07 +0100

    tools_free

---
+++ philo/tools_free.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_free.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 10:46:42 by urlooved          #+#    #+#             */
+/*   Updated: 2025/03/10 11:06:39 by urlooved         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+// will free the struct table and all philosophers inside.
+void	free_table(t_table *table)
+{
+	unsigned int	i;
+
+	if (!table)
+		return ;
+	if (table->fork_locks != NULL)
+		free(table->fork_locks);
+	if (table->philos != NULL)
+	{
+		i = 0;
+		while (i < table->amount_philos)
+		{
+			if (table->philos[i] != NULL)
+				free(table->philos[i]);
+			i++;
+		}
+		free(table->philos);
+	}
+	free(table);
+}
+// Will free all mutexes in table and in philos
+void	free_mutexes(t_table *table)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < table->amount_philos)
+	{
+		pthread_mutex_destroy(&table->fork_locks[i]);
+		pthread_mutex_destroy(&table->philos[i]->meal_time_lock);
+		i++;		
+	}
+	pthread_mutex_destroy(&table->write_lock);
+	pthread_mutex_destroy(&table->sim_stop_lock);
+}
+
