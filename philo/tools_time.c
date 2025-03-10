283461482e95a4e387098b89c3192e30e1b3fc2f
rita <runyalen88@duck.com>
2025-03-10 11:36:25 +0100

    sim_start_delay

--- philo/tools_time.c
+++ philo/tools_time.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   time.c                                             :+:      :+:    :+:   */
+/*   tools_time.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 14:40:00 by urlooved          #+#    #+#             */
-/*   Updated: 2025/03/08 14:47:13 by urlooved         ###   ########.fr       */
+/*   Updated: 2025/03/10 11:35:05 by urlooved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,14 @@ time_t	get_current_time(void)
 // // {
 // //     return (printf("Current time in ms: %ld\n", get_current_time()), 0);
 // // }
+
+/*
+- Will delay the simulation like that all thread are synchronized 
+- This ensures t_stop_program thread is synchronized with 
+	the philosopher threads.
+*/
+void	sim_start_delay(time_t start_meeting_at)
+{
+	while (get_current_time() < start_meeting_at)
+		continue ;									// will loop until the condition is reached
+}
