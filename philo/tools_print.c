dddf16f5cf4c7c4532a87e2ed1d3a8baf230a694
rita <runyalen88@duck.com>
2025-03-12 15:16:08 +0100

    print_statement updated without strcmp

--- philo/tools_print.c
+++ philo/tools_print.c
@@ -6,7 +6,7 @@
 /*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 10:44:32 by urlooved          #+#    #+#             */
-/*   Updated: 2025/03/12 10:50:23 by urlooved         ###   ########.fr       */
+/*   Updated: 2025/03/12 15:01:26 by urlooved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ void	print_statement(t_philo *philo, char *status)
 	int		philo_id;
 
 	pthread_mutex_lock(&philo->table->write_lock);
-	if (should_sim_end(philo->table) == true)
+	if (should_sim_end(philo->table) == true)					// if end of the process we cut the acces to write in the terminal
 	{
 		pthread_mutex_unlock(&philo->table->write_lock);
 		return ;
 	}
-	now_at = get_current_time() - philo->table->start_meeting_at;
+	now_at = get_time_in_ms() - philo->table->start_meeting_at;
 	philo_id = philo->id + 1;
-	if (ft_strcmp(status, "DIED") == 0)
+	if (status[0] == 'D')
 		printf("%ld %d %s\n", now_at, philo_id, "died");
-	else if (ft_strcmp(status, "EATING") == 0)
+	else if (status[0] == 'E')
 		printf("%ld %d %s\n", now_at, philo_id, "is eating");
-	else if (ft_strcmp(status, "SLEEPING") == 0)
+	else if (status[0] == 'S')
 		printf("%ld %d %s\n", now_at, philo_id, "is sleeping");
-	else if (ft_strcmp(status, "THINKING") == 0)
+	else if (status[0] == 'T')
 		printf("%ld %d %s\n", now_at, philo_id, "is thinking");
-	else if (ft_strcmp(status, "FORK_0") == 0 || ft_strcmp(status, "FORK_1") == 0)
+	else if (status[0] == 'F')
 		printf("%ld %d %s\n", now_at, philo_id, "has taken a fork");
 	pthread_mutex_unlock(&philo->table->write_lock);
 }
