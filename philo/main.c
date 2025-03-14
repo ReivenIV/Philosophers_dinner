949a606733fba9b6795da3fa605dee44a3ae8c65
rita <runyalen88@duck.com>
2025-03-14 10:29:13 +0100

    main updates

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 16:02:28 by urlooved          #+#    #+#             */
-/*   Updated: 2025/03/14 09:56:56 by urlooved         ###   ########.fr       */
+/*   Updated: 2025/03/14 10:29:04 by urlooved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,14 @@ int	main(int ac, char **av)
 	table = NULL;
 	if (ac < 5 || ac > 6)
 		return (printf("wrong amount ac"));
-	// parser
 	if (!handler_parser(ac, av))
 		exit (1);
-
-	if (av[1][0] == '1')						// in case we have only 1 philo we wait till it die. we don't init anything.
-		return (usleep(nbs_atoi(av[2])), printf("%s 1 died\n", av[2]), 0); //TODO maybe.. 
-	// TODO if only 1philo it should die automatically
-		// inits: table g_mutexes, forks, array of philosophers
 	table = init_table_philos(ac, av);
 	if (!table)
 		exit (1);
-	
-	// Seeters, routines(eat, think, sleep), all processes threads / mutex activities
-	//TODO eat, sleep, think time
-	
-	//TODO end process free all. 
-	
+	if (!begin_process(table))
+		exit (1);
+	end_process(table);
 	return (0);
 }
 //! leaks when closing the process (in infinit loop mode)
