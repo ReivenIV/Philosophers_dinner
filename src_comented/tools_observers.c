7f7de293f355c0246a18334b1750eab5fe1499d6
rita <runyalen88@duck.com>
2025-03-16 15:29:45 +0100

    fixbug "are_all_conditions_reached"

--- src_comented/tools_observers.c
+++ src_comented/tools_observers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools_observers.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: urlooved <urlooved@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 15:49:13 by urlooved          #+#    #+#             */
-/*   Updated: 2025/03/14 15:27:38 by urlooved         ###   ########.fr       */
+/*   Updated: 2025/03/16 14:43:03 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,10 @@ bool	is_philo_dead(t_phi *philo)
 	if ((current_time - philo->last_meal_at) >= philo->table->t_t_die)
 	{
 		print_statement(philo, "DIED");
-		update_sim_should_stop(philo->table, true);			// Will update table->sim_should_stop BOOL to TRUE
 		pthread_mutex_unlock(&philo->phi_action_lock);
 		return (true);										// yep someone died
 	}
-	return (false);											// all good we can continue wiht the process.
+	return (false);											// all philosophers are alive. (continue the process)
 }
 
 // Will check if philos are alive & if everyone had ete the min amount of meals.
@@ -48,26 +47,26 @@ bool	are_all_conditions_reached(t_table *table)
 {
 	unsigned int	i;
 	bool			amount_min_meals_reached;
+	bool			should_stop;
 
-	if (table->min_amount_meals == -1)						// REMINDER: amount_of_meals is optional is if -1 then we loop forever.
-		return (false);
 	i = 0;
 	amount_min_meals_reached = true;
+	should_stop = false;
 	while (i < table->amount_philos)
 	{
 		pthread_mutex_lock(&table->philos[i]->phi_action_lock);
 		if (is_philo_dead(table->philos[i]))
-			return (true);
-		if (table->philos[i]->times_ate < (unsigned int)table->min_amount_meals)
-			amount_min_meals_reached = false;
+			should_stop = true;															// Will check if a philo died or not a philo if yes stop the procees (its announced at func "is_philo_dead")
+		if (table->min_amount_meals != -1)
+			if (table->philos[i]->times_ate < (unsigned int)table->min_amount_meals)
+				amount_min_meals_reached = false;										// if min_amount_meals not reached then we continue the process. 
 		pthread_mutex_unlock(&table->philos[i]->phi_action_lock);
 		i++;
 	}
-	if (amount_min_meals_reached == true)
+	if (amount_min_meals_reached == true || should_stop == true)
 	{
 		update_sim_should_stop(table, true);
 		return (true);
 	}
 	return (false);
 }
-
